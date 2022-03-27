93bf91181017382259fb05816003d381b8cd5244
CygnusX-26 <42686923+CygnusX-26@users.noreply.github.com>
2022-03-27 15:45:47 -0700

    bugfixes

--- Music.py
+++ Music.py
@@ -1,4 +1,3 @@
-from asyncio import tasks
 import asyncio
 import os
 import discord
@@ -7,6 +6,7 @@ from discord.utils import get
 import youtube_dl
 import validators
 import requests
+import base64
 from pprint import pprint
 
 
@@ -18,7 +18,6 @@ class Music(commands.Cog):
     
 
     async def check_queue(self, ctx):
-        print(self.queue)
         if self.queue != []:
             voice = ctx.guild.voice_client
             await self.play_song(ctx, voice, self.queue.pop(0))
@@ -102,12 +101,20 @@ class Music(commands.Cog):
     async def stop(self, ctx):
         voice = get(self.bot.voice_clients, guild=ctx.guild)
         if voice and voice.is_playing():
+            self.queue = []
             voice.stop()
-            await ctx.send("Stopped the current track")
+            await ctx.send("Stopped the queue")
         else:
             await ctx.send("I am not playing anything")
     
-
+    @commands.command()
+    async def skip(self, ctx):
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        if (voice and voice.is_playing()):
+            voice.stop()
+            await ctx.send("Skipped the current track")
+        else:
+            await ctx.send("I am not playing anything")
 
 
 
@@ -115,14 +122,15 @@ class Music(commands.Cog):
 
     @commands.command()
     async def track(self, ctx, * , query):
+        access = os.getenv('SP_ID') + ":" + os.getenv('SP_SECRET')
+        response = requests.post('https://accounts.spotify.com/api/token', data = {'grant_type':'client_credentials'}, headers = {'Authorization':'Basic ' + base64.b64encode(access.encode()).decode()}).json()
         id = query[34:]
-        playlist = []
         headers = {
-        'Authorization': f'Bearer {os.getenv("SP_TOKEN")}',
+        'Authorization': f'Bearer {response["access_token"]}',
         }
         data = requests.get(f"https://api.spotify.com/v1/playlists/{id}", headers=headers).json()
         for i in range(len(data['tracks']['items'])):
-            playlist.append(data['tracks']['items'][i]['track']['album']['name'])
+            self.queue.append(data['tracks']['items'][i]['track']['album']['name'])
         try:
             vc = ctx.author.voice.channel
         except AttributeError:
@@ -135,17 +143,12 @@ class Music(commands.Cog):
             voice = await vc.connect()
         else:
             voice = get(self.bot.voice_clients, guild=ctx.guild)
-        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True', 'default_search':'ytsearch'}
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        for i in range(len(playlist)):
-            if not voice.is_playing():
-                with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-                    info = ydl.extract_info(playlist[i], download=False)
-                    URL = info['entries'][0]['formats'][0]['url']
-                    voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
-                    voice.is_playing()
-                    
-            else:
-                await ctx.send("Already playing a playlist")
-                return
+        if (not voice.is_playing()):
+            await self.play_song(ctx, voice, self.queue.pop(0))
+    
+    @commands.command()
+    async def queue(self, ctx):
+        for i in self.queue:
+            await ctx.send(i)
+            
         
