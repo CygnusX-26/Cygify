1850d479fa1767b8256b4d145f98f02a70c708d4
CygnusX-26 <42686923+CygnusX-26@users.noreply.github.com>
2022-03-30 17:52:45 -0700

    added Bot cog

--- discordBot.py
+++ discordBot.py
@@ -2,16 +2,22 @@ import os
 import discord
 from discord.ext import commands
 from Music import Music
+from Bot import Bot
 
 intents  = discord.Intents.all()
 client = commands.Bot(command_prefix = '-', intents=intents)
 client.remove_command('help')
 
 client.add_cog(Music(client))
+client.add_cog(Bot(client))
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    await client.change_presence(
+        status=discord.Status.online,
+        activity=discord.Game("use -help for a list of commands!")
+    )
 
 
 client.run(os.getenv('TOKEN'))
